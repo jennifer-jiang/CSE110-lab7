9cade5214f7752f21463d37f6158c64711e0a27d
jennifer-jiang <51929459+jennifer-jiang@users.noreply.github.com>
2022-11-12 23:23:08 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,15 +60,11 @@ function initializeServiceWorker() {
         const registration = await navigator.serviceWorker.register("./sw.js", {
           scope: "./",
         });
-        if (registration.installing) {
-          console.log("Service worker installing");
-        } else if (registration.waiting) {
-          console.log("Service worker installed");
-        } else if (registration.active) {
-          console.log("Service worker active");
+        if (registration) {
+          console.log("Registration was successful");
         }
       } catch (error) {
-        console.error(`Registration failed with ${error}`);
+        console.error("Registration failed with ${error}");
       }
     });
   }
